361574d874b4c0a830c4d4a7fd07049f61cd9b8a
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-12-23 00:14:22 +0500

    add

--- my-project/src/components/AddToCard.jsx
+++ my-project/src/components/AddToCard.jsx
@@ -12,19 +12,19 @@ export function AddToCard({
   return (
     <>
 
-      <div className="main  bg-white  w-[270px] h-[350px] m-8 ">
-        <div className="img w-[270px] h-[250px] bg-[#F5F5F5] relative center ">
+      <div className="main  bg-lime-300  w-[170px] h-[285px] m-8 ">
+        <div className="img w-[170px] h-[150px] bg-[#F5F5F5] relative center ">
           <button
-            className=" w-[55px] h-[26px] bg-myTheme rounded text-white absolute left-3 right-4 bottom-1 top-3 ;
+            className=" w-[35px] h-[16px] text-xs bg-myTheme rounded text-white absolute left-1 right-4 bottom-1 top-2 ;
 "
           >
             {discount}
           </button>
-          <img className=" w-[172px] h-[152px] m-10 hover:scale-125" src={game} alt="" />
+          <img className=" w-[150px] h-[120px] m-10 hover:scale-125" src={game} alt="" />
         </div>
-        <div className="myp w-[201px] h-[84px] gap-2 my-3">
-          <h2 className="w-[201] h-6 text-base font-medium hover:text-myTheme">{productName}</h2>
-          <p className="flex gap-4 my-2 w-[100px] h-6  text-base font-medium text-myTheme">
+        <div className="myp w-[170px] h-[120px] bg-red-700 gap-2 my-3">
+          <h2 className="w-[201] h-6 text-base bg-rose-500 font-medium hover:text-myTheme">{productName}</h2>
+          <p className="flex gap-4 my-1 w-[100px] h-6  bg-yellow-600 text-base font-medium text-myTheme">
             <ins className="no-underline"> {newPrice}</ins> {"  "}
             <s className="text-slate-400 "> {delPrice}</s>
           </p>
